590f4624994ab52c71a6369dfc685f5e641c9ccc
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2022-09-15 22:19:31 +0530

    Generate pattern
    /** Generate the pattern for given number .
     * example : 
     * input: 
     * 4
     * output : 
     *  *****
     *   ****
     *    ***
     *     **
     *

---
+++ CODING  PRACTICES OUR SELF/Generate_Pattern6.java
@@ -0,0 +1,33 @@
+/** Generate the pattern for given number .
+ * example : 
+ * input: 
+ * 4
+ * output : 
+ *  *****
+ *   ****
+ *    ***
+ *     **
+ *      *
+ */
+import java.util.Scanner;
+
+public class GeneratePattern6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int n = scan.nextInt();
+      
+        for(int i=n; i>0; i--){
+
+            for (int j=n; j>i;j--)
+            {
+                System.out.print(" ");
+            }   
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+              
+            }
+            System.out.println();
+        }
+    }
+}
