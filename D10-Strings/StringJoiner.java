c8dd2a48cd68c1d729b021ca3a4cd813b5034e97
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2022-10-08 00:27:18 +0530

    To marge two string joiner and add prefix & suffix

--- D10-Strings/StringJoiner.java
+++ D10-Strings/StringJoiner.java
@@ -8,12 +8,22 @@
  *
  *
  */
+/**
+ * Java added a new final class StringJoiner in java.util package. It is used to construct a sequence of characters separated by a delimiter. Now, you can create string by passing delimiters like comma(,), hyphen(-) etc. You can also pass prefix and suffix to the char sequence.
+ *
+ * Output like that :
+ * [A,B,C]
+ * P:Q:R
+ * X-Y-Z
+ *[A,B,C,D,P:Q:R]
+ *
+ */
 
 import java.util.StringJoiner;
 
 public class JavaCommaColon {
     public static void main(String[] args) {
-        StringJoiner str1 = new StringJoiner(",");
+        StringJoiner str1 = new StringJoiner("," ,"[" ,"]");
         str1.add("A").add("B").add("C").add("D");
         StringJoiner str2 = new StringJoiner(":");
         str2.add("P").add("Q").add("R");
@@ -23,5 +33,8 @@ public class JavaCommaColon {
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
+
+        str1.merge(str2);
+        System.err.println(str1);
     }
 }
