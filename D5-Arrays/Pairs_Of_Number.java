83f01f0c64df06094ab23d9cacacd09b5015b91d
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2022-10-30 00:12:19 +0530

    added count of pairs

--- D5-Arrays/Pairs_Of_Number.java
+++ D5-Arrays/Pairs_Of_Number.java
@@ -7,12 +7,17 @@ public class AllPossiblePairs {
 
     }
     public static void pairs(int[] arr){
+        //to find numbers of pairs
+        int cp = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j <arr.length; j++) {
-                System.out.print("(" + i+", " + j + ") ;");
+            for (int j = i+1; j <arr.length; j++) {
+                System.out.print("(" + arr[i]+", " + arr[j] + ") ;");
+                cp++;
 
             }
             System.out.println();
         }
+        System.out.println(cp);
     }
 }
