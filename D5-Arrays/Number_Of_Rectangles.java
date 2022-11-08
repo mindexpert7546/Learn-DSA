929037b15110eda24686c497df3b87b71d55a003
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2022-11-08 16:30:31 +0530

    1725. Number Of Rectangles,Largest Square leetcode

--- D5-Arrays/Number_Of_Rectangles.java
+++ D5-Arrays/Number_Of_Rectangles.java
@@ -1,4 +1,10 @@
-int temp[] = new int[rectangles.length];
+//Question link : https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/description/
+
+
+
+class Solution {
+    public int countGoodRectangles(int[][] rectangles) {
+        int temp[] = new int[rectangles.length];
         int max = Integer.MIN_VALUE;
         int res =0;
 
@@ -11,17 +17,22 @@ int temp[] = new int[rectangles.length];
             }
         }
 
+        //now we will find max 
 
         for(int i=0; i<temp.length; i++){
             if(temp[i]>max){
-                max= temp[i];
+                max=temp[i];
             }
         }
 
+        // to find result 
 
-        for(int i=0; i<temp.length; i++){
+        for(int i=0;i<temp.length; i++){
             if(temp[i]==max){
                 res++;
             }
         }
+
         return res;
+    }
+}
