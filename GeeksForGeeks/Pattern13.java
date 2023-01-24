8281d4bc944505a359e74921d5ba89fb00c0632a
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-01-24 23:49:03 +0530

    Pattern 13

---
+++ GeeksForGeeks/Pattern13.java
@@ -0,0 +1,43 @@
+/**
+https://practice.geeksforgeeks.org/problems/triangle-pattern-1661718712/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_13
+*/
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// Back-end complete function Template for Java
+
+class Solution {
+
+    void printTriangle(int n) {
+        
+        //code 
+        int k = 1;
+        for(int i=0; i<n; i++){
+           
+            for(int j=0; j<=i; j++){
+                System.out.print(k + " ");
+                k++;
+               
+            }
+            System.out.println();
+        }
+    }
+}
