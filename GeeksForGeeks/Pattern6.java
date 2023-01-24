e99a6e83f6154b6de3615bdd188d7fbd76c476ab
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-01-24 23:26:21 +0530

    Pattern 6

---
+++ GeeksForGeeks/Pattern6.java
@@ -0,0 +1,39 @@
+/**
+https://practice.geeksforgeeks.org/problems/triangle-number-1661489840/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_6
+
+*/
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj=new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        for(int i=n; i>0; i--){
+            int k=1;
+            for(int j=0; j<i; j++){
+                System.out.print(k + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
