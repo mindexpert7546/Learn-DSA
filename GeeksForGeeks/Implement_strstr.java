52c2d23b2571fbee67e933151bb0165b93dd538e
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-01-31 00:34:17 +0530

    Implement strstr

---
+++ GeeksForGeeks/Implement_strstr.java
@@ -0,0 +1,48 @@
+/**
+Implement strstr
+
+https://practice.geeksforgeeks.org/batch/dsa-4/track/DSASP-Strings/problem/implement-strstr
+
+*/
+
+//{ Driver Code Starts
+import java.util.*;
+
+
+class Implement_strstr
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String a = line.split(" ")[0];
+			String b = line.split(" ")[1];
+			
+			GfG g = new GfG();
+			System.out.println(g.strstr(a,b));
+			
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class GfG
+{
+    //Function to locate the occurrence of the string x in the string s.
+    int strstr(String s, String x)
+    {
+       // Your code here
+       if(s.contains(x)){
+           return s.indexOf(x);
+       }
+       
+       return -1;
+       
+    }
+}
