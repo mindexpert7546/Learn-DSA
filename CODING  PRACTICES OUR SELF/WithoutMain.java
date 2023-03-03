a339da92206bd85505331f9a03fae445c55d3b29
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-03-04 00:23:41 +0530

    to execute java code without using main method

---
+++ CODING  PRACTICES OUR SELF/WithoutMain.java
@@ -0,0 +1,12 @@
+package Self;
+
+public class Hello {
+    public static void main(String[] args) {
+
+    }
+
+    // static block
+    static {
+        System.out.println("Hello world");
+    }
+}
