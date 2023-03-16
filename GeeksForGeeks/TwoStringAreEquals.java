590ff9b660a04a8d436436aae3eef4854bf8c670
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-03-16 23:25:14 +0530

    Check if two arrays are equal or not

---
+++ GeeksForGeeks/TwoStringAreEquals.java
@@ -0,0 +1,64 @@
+//Check if two arrays are equal or not
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+      Arrays.sort(A);
+      Arrays.sort(B);
+      boolean flag = false;
+      for(int i=0; i<N; i++){
+          if(A[i] != B[i]){
+              flag = true;
+          }
+      }
+      
+      if(flag == true){
+          return false;
+      }
+      return true;
+    }
+}
