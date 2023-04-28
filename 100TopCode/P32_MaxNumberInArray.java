09ed438ed02cfbb5773ea92da5e8a8e06772fc87
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-04-29 00:16:14 +0530

    Create P32_MaxNumberInArray.java

---
+++ 100TopCode/P32_MaxNumberInArray.java
@@ -0,0 +1,29 @@
+package PrepInsta;
+
+import java.util.Scanner;
+
+public class P32_MaxNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = {3,54,2,6,87,76,9};
+        int l = arr.length-1;
+        int max = arr[0];
+       int res =  maxNumber(arr,l,max);
+        System.out.println(res);
+    }
+
+    // function for find the maximum number in array by recursion
+
+    public static int maxNumber(int[] arr, int l, int max){
+        if(l==0){
+            return max;
+        }
+        if(l>0){
+            if(arr[l]>max){
+                max = arr[l];
+            }
+        }
+
+        return maxNumber(arr,l-1,max);
+    }
+}
