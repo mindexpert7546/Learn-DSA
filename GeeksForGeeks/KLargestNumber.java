1d890fe04c032b03f36a6812a1b4659c354ebb2f
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-06-13 22:20:06 +0530

    Create KLargestNumber.java

---
+++ GeeksForGeeks/KLargestNumber.java
@@ -0,0 +1,49 @@
+
+
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        // code here
+        int[] ans=new int[k];
+        Arrays.sort(arr);
+        int ind=0;
+        n=n-1;
+        for(int i=0; i<k; i++){
+            ans[ind++]=arr[n--];
+        }
+        return ans;
+
+    }
+}
