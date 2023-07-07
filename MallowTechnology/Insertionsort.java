fa501d42b6d9f34257421dda4e94770676b42687
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-07-07 11:40:32 +0530

    Create Insertionsort.java

---
+++ MallowTechnology/Insertionsort.java
@@ -0,0 +1,67 @@
+
+//https://practice.geeksforgeeks.org/problems/insertion-sort/1
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 new Solution().insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+       // Your code here
+       return;
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+      //code here
+      
+      for(int i=1; i<n; i++){
+          int e = arr[i];
+          int j=i-1;
+          
+          while(j>=0 && e<arr[j]){
+              arr[j+1]=arr[j];
+              j--;
+          }
+          
+          arr[j+1]=e;
+          
+      }
+  }
+}
