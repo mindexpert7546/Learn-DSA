b32710e9ee6d987c94c059b6f2cbc58ae387fde2
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-07-16 23:00:08 +0530

    Create SearchTwoDMatrix.java

---
+++ D8-Matrix/SearchTwoDMatrix.java
@@ -0,0 +1,19 @@
+
+//https://leetcode.com/problems/search-a-2d-matrix/description/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+         boolean flag = false;
+
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length; j++){
+                if(matrix[i][j]==target){
+                    flag=true;
+                    break;
+                }
+            }
+        }
+
+        return flag;
+    }
+}
