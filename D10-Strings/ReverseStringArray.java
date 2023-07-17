348a0dc5d36138d256c020bf491a436226254b46
Kundan Kumar <89348788+mindexpert7546@users.noreply.github.com>
2023-07-17 16:10:56 +0530

    Create ReverseStringArray.java

---
+++ D10-Strings/ReverseStringArray.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/reverse-string/submissions/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+
+        while(i<=j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
